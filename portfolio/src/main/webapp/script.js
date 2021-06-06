ce5f4d701d50687167c477775a19fe7178b89ec7
Gort <gmarsh0218@gmail.com>
2021-06-05 21:39:09 -0400

    added js code to get last/current song played/playing from Last.fm api

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,73 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+// function all elements that need js
+function onLoad() {
+  getLastSong();
+}
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+function timeSince(date) {
+  // difference of time between initial date and now in seconds, divide by 1000 because js uses ms for time.
+  let timeElapsed = Math.floor((new Date() - date) / 1000);
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const unitsInSecs = {
+    'year': 31536000,
+    'month': 2419200,
+    'week': 604800,
+    'day': 86400,
+    'hour': 3600,
+    'minute': 60,
+    'second': 1
+  };
+
+  let returnString = '';
+  for (const [key, val] of Object.entries(unitsInSecs)) {
+    if(Math.floor(timeElapsed / val) >= 1){
+      returnString = `\`172850 % ${val} = ${Math.floor(172850 / val)} ${key}`
+      break;
+  }
+
+    if(!returnString.startsWith('1 ')){
+      returnString += 's'
+    }
+
+    return returnString;
+}
+
+
+}
+
+function getLastSong(){
+  const url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=gmarsh0218&api_key=a228b6b2b58f233537d93206e9b768c5&format=json&limit=1';
+  fetchAsync(url, (jsonData) => {
+
+    const trackData = jsonData.recenttracks.track[0];
+    const artistName = trackData.artist["#text"];
+    const songName = trackData.name;
+    const albumName = trackData.album["#text"];
+
+    let listeningInfo;
+
+    if(trackData["@attr"] && trackData["@attr"].nowplaying === "true"){
+      listeningInfo = "I am currently listening to " + songName + " by " + artistName + "." ;
+    }
+    else{
+      let timeSinceSongPlayed = timeSince(trackData.date["uts"]);
+      listeningInfo = "I was listening to " + songName + " by " + artistName +",  "+ timeSinceSongPlayed + " ago.";
+    }
+    // set listening info
+    document.getElementById("song-info").innerText = listeningInfo;
+    //set album picture and alt text for accessibility purposes.
+    document.getElementById("album-art").src = trackData.image[2]["#text"];
+    document.getElementById("album-art").alt = albumName;
+  });
+
+
+  //fetchAsync() taken from https://stackoverflow.com/a/38297729
+  async function fetchAsync (url, callback) {
+    let response = await fetch(url);
+    let data = await response.json();
+    callback(data);
+  }
+
 }
