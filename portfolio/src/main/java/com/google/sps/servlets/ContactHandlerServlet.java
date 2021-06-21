e263a40cd3f1021af9e2a53a901e837b559f43de
Garrett Marsh <gmarsh0218@gmail.com>
2021-06-21 11:57:19 -0400

    Added output to the Servlet

--- portfolio/src/main/java/com/google/sps/servlets/ContactHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactHandlerServlet.java
@@ -1,6 +1,14 @@
 package com.google.sps.servlets;
 
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
 import java.io.IOException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +18,16 @@ import javax.servlet.http.HttpServletResponse;
 public class ContactHandlerServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
 		// Get values from the form
-		String name = request.getParameter("");
+		String name = request.getParameter("name-input");
+		String email = request.getParameter("email-input");
+		String message = request.getParameter("message-input");
+
+		// Print message successful message received!
+		System.out.println("New Message Received from " + name + "(" + email + ")":);
+		System.out.println(message);
+
 
 	}
 }
