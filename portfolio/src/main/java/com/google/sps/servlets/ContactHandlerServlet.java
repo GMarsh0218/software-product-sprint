9d7e419e43cdd28d08030bc29be066e5fce7c515
Garrett Marsh <gmarsh0218@gmail.com>
2021-06-22 10:45:12 -0400

    Add print functionality and redirects to homepage

--- portfolio/src/main/java/com/google/sps/servlets/ContactHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactHandlerServlet.java
@@ -1,21 +1,14 @@
 package com.google.sps.servlets;
 
 
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.QueryResults;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
 import java.io.IOException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/contact-form-handler")
-public class ContactHandlerServlet {
+public class ContactHandlerServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 
@@ -25,9 +18,14 @@ public class ContactHandlerServlet {
 		String message = request.getParameter("message-input");
 
 		// Print message successful message received!
-		System.out.println("New Message Received from " + name + "(" + email + ")":);
+		System.out.println("New Message Received from " + name + "(" + email + "):");
 		System.out.println(message);
 
-
+		//redirect to homepage
+		try {
+			response.sendRedirect("/index.html");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
